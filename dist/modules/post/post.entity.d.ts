2cd9df9a3299b12444f51b6479031c101749d3af
thoaiky1992 <thoaiky1992@gmail.com>
2021-02-01 21:44:03 +0700

    update

--- dist/modules/post/post.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from 'sequelize-typescript';
-import { User } from '../users/user.entity';
-export declare class Post extends Model<Post> {
-    title: string;
-    body: string;
-    createdAt: Date;
-    userId: number;
-    user: User[];
-}
